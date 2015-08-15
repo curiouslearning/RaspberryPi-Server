7b05edc700344e6eaf318ef6016d338f91987ef8
Jason Krone <jasonkrone@me.com>
2015-08-15 01:31:17 +0000

    added logging to file mover

--- file_mover/file_mover.sh
+++ file_mover/file_mover.sh
@@ -6,15 +6,16 @@
 #
 
 source /home/pi/RaspberryPi-Server/logger.sh
+source /home/pi/RaspberryPi-Server/counter.sh
 source /home/pi/RaspberryPi-Server/config.sh
 
 
 function main() {
-	echo "from file_mover.sh $(date)" >> "$file_mover_log"
+	echo "running file_mover.sh on $(date)" >> "$file_mover_log"
 	local success=0
 
 	# are there files to move ?
-	if [[ $( ls -c "$archive_dir" | wc -l ) -gt 0 ]]; then
+	if [[ $( num_files_in_dir "$archive_dir" ) -gt 0 ]]; then
 		move_files
 		success=$?
 	fi
@@ -31,32 +32,35 @@ function move_files() {
 	# array of files to be moved
 	archived_files=( "$archive_dir"* )
 	success=$?
+	log_status $success "getting list of archives" "$file_mover_log"
 
 	if [[ $success -eq 0 ]]; then 
 		sudo cp $archive_dir* "$file_mover_temp"
 		success=$?
+		log_status $success "copying archives to temp" "$file_mover_log"
 	fi 	
 
 	if [[ $success -eq 0 ]]; then 
 		# copy files from temp to usb
 		sudo cp $file_mover_temp* $usb_mnt_point	
 		success=$?
+		log_status $success "copying archives to usb" "$file_mover_log"
 	fi
 	
 	if [[ $success -eq 0 ]]; then 
 		# move files from temp to backups
 		sudo mv "$file_mover_temp"* "$backup_dir"
 		success=$?
+		log_status $success "backing up archives" "$file_mover_log"
 	fi
 
 	if [[ $success -eq 0 ]]; then 
 		# remove backedup files from archive
-		for file in "${archived_files[@]}"; do
-			sudo rm "$file"
-		done
+		sudo rm ${archived_files[@]}
+		log_status $success "removing backups from archive" "$file_mover_log"
 	fi
 
-	return $?
+	return "$success"
 }
 
 
