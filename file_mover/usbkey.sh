753cd8968727b21a910bb75728ebe435ccca95f6
Jason Krone <jasonkrone@me.com>
2015-08-19 19:02:40 +0000

    modified usbkey to run update script on usb

--- file_mover/usbkey.sh
+++ file_mover/usbkey.sh
@@ -8,37 +8,54 @@
 ACTION=$(expr "$ACTION" : "\([a-zA-Z]\+\).*")
 
 source /home/pi/RaspberryPi-Server/config.sh
+tablet_update_script="/mnt/usb/globallit_tablet_updates.sh"
 
 function main() {
+	local success=0
+
 	if [[ "$ACTION" = "add" ]]; then
-		move_archives_to_usb
+		mount_file_mover_usb
+		if [[ "$?" -eq 0 ]]; then
+			# move archives to usb 
+			/home/pi/RaspberryPi-Server/file_mover/file_mover.sh 
+
+			# run tablet updates if it exist on usb
+			[ -f "$tablet_update_script" ] && "$tablet_update_script"
+		else
+			# usb not mounted
+			success=1	
+		fi
 	else
 	# usb is being removed
 		sudo umount "$usb_mnt_point" 
+		success=$?
 	fi
-	exit $?
+	exit "$success"
 }
 
-# purp: mounts usb if usb has the correct id file and then moves files to usb
+
+# purp: mounts the node symlinked to the usb mnt point if 
+# that node contains the correct usb_id file, otherwise unmounts node
 # args: none
-# rets: 0 if successful
-function move_archives_to_usb() {
+# rets: 0 if the node is mounted and 1 otherwise
+function mount_file_mover_usb() {
+	local success=0
 	sudo mount "$usb_mnt_point" 
+	
+
 
 	if [[ "$?" -eq 0 ]]; then
 		# check that this is a file_mover usb
-		if [[ $( cat /mnt/usb/usb_id ) = "file_mover_usb" ]]; then
-			# move files to usb
-			/home/pi/RaspberryPi-Server/file_mover/file_mover.sh 
-		else
+		if [[ $( cat /mnt/usb/usb_id ) != "file_mover_usb" ]]; then
 			# usb is not meant to store files
-			sudo umount "$usb_mnt_point"	
+			sudo umount "$usb_mnt_point"
+			success=1
 		fi
-		return 0
 	else
-		echo "problem mounting usb" >> "$file_mover_log" 
-		return 1
+		success=1	
 	fi
-}
+
+	return "$success"
+}	
 
 main
