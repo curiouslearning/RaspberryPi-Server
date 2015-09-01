84ddcd2e921fa5d6fa3cb9d9fadc0866c0266537
Jason Krone <jasonkrone@me.com>
2015-09-01 19:29:25 +0000

    changed usbkey to not be dependant on config

--- file_mover/usbkey.sh
+++ file_mover/usbkey.sh
@@ -7,8 +7,8 @@
 
 ACTION=$(expr "$ACTION" : "\([a-zA-Z]\+\).*")
 
-source /home/pi/RaspberryPi-Server/config.sh
 tablet_update_script="/mnt/usb/globallit_tablet_updates.sh"
+mnt_point="/mnt/usb/"
 
 function main() {
 	local success=0
@@ -17,7 +17,7 @@ function main() {
 		mount_file_mover_usb
 		if [[ "$?" -eq 0 ]]; then
 			# move archives to usb 
-			/home/pi/RaspberryPi-Server/file_mover/file_mover.sh 
+			./file_mover.sh 
 
 			# run tablet updates if it exist on usb
 			[ -f "$tablet_update_script" ] && "$tablet_update_script"
@@ -27,7 +27,7 @@ function main() {
 		fi
 	else
 	# usb is being removed
-		sudo umount "$usb_mnt_point" 
+		sudo umount "$mnt_point" 
 		success=$?
 	fi
 	exit "$success"
@@ -40,15 +40,13 @@ function main() {
 # rets: 0 if the node is mounted and 1 otherwise
 function mount_file_mover_usb() {
 	local success=0
-	sudo mount "$usb_mnt_point" 
+	sudo mount "$mnt_point" 
 	
-
-
 	if [[ "$?" -eq 0 ]]; then
 		# check that this is a file_mover usb
 		if [[ $( cat /mnt/usb/usb_id ) != "file_mover_usb" ]]; then
 			# usb is not meant to store files
-			sudo umount "$usb_mnt_point"
+			sudo umount "$mnt_point"
 			success=1
 		fi
 	else
