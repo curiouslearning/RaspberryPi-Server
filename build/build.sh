976bdbb901dad6558494345e33b7bcce1cba0e34
Jason Krone <jasonkrone@me.com>
2015-09-04 22:42:52 +0000

    changed path for build

--- build/build.sh
+++ build/build.sh
@@ -6,21 +6,21 @@
 
 # import logger
 source ../logger.sh
-build_log="build_log.txt"
+build_log_path="build/build_log.txt"
 
 function main() {
 	../webserver/setup_webserver.sh
-	log_status $? "setting up webserver" "$build_log"
+	log_status $? "setting up webserver" "$build_log_path"
 
 	../archiver/setup_archiver.sh
-	log_status $? "setting up archiver" "$build_log"
+	log_status $? "setting up archiver" "$build_log_path"
 
 	../file_mover/setup_file_mover.sh
-	log_status $? "setting up file_mover" "$build_log"
+	log_status $? "setting up file_mover" "$build_log_path"
 
 	# setup cronjobs TODO: modify paths for cron jobs
 	crontab ../pi_server_crontab
-	log_status $? "setting up crontab" "$build_log"
+	log_status $? "setting up crontab" "$build_log_path"
 }
 
 main
